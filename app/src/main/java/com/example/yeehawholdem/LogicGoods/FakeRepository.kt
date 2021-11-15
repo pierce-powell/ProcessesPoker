80fc2a2c464749554feefcdd2ef0bec5e8f53dba
MistbornShadow <64308098+MistbornShadow@users.noreply.github.com>
2021-11-15 06:35:28 -0500

    Update FakeRepository.kt
    Mere name change, just so a contribution is made in my name.

--- app/src/main/java/com/example/yeehawholdem/LogicGoods/FakeRepository.kt
+++ app/src/main/java/com/example/yeehawholdem/LogicGoods/FakeRepository.kt
@@ -6,15 +6,15 @@ class FakeRepository {
             Player("Pierce", 2000f),
             Player("Nick", 3000f),
             Player("Luke", 55000f),
-            Player("Isreal", 3300f),
+            Player("Israel", 3300f),
             Player("Pierce", 2000f),
             Player("Nick", 3000f),
             Player("Luke", 55000f),
-            Player("Isreal", 3300f),
+            Player("Israel", 3300f),
             Player("Pierce", 2000f),
             Player("Nick", 3000f),
             Player("Luke", 55000f),
-            Player("Isreal", 3300f)
+            Player("Israel", 3300f)
         )
     }
 }
