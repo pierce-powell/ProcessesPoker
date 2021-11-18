77619dd060cb91862e3ebec181b5a7bc76fd7a58
MistbornShadow <64308098+MistbornShadow@users.noreply.github.com>
2021-11-18 13:50:06 -0500

    Added a test case function folder.

---
+++ app/src/test/java/com/example/yeehawholdem/Testing/CheckHandTest.kt
@@ -0,0 +1,28 @@
+package com.example.yeehawholdem.Testing
+
+import junit.framework.TestCase
+
+class CheckHandTest : TestCase() {
+
+    fun testIsRoyalFlush() {}
+
+    fun testIsStraightFlush() {}
+
+    fun testIsFourOfAKind() {}
+
+    fun testIsFullHouse() {}
+
+    fun testIsStraight() {}
+
+    fun testIsFlush() {}
+
+    fun testIsThreeOfAKind() {}
+
+    fun testIsTwoPairs() {}
+
+    fun testIsPair() {}
+
+    fun testBestHand() {}
+
+    fun testGetCurHandValue1() {}
+}
