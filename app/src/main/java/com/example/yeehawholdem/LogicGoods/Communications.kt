54b37233a110d05a7686579efd2b8427a8f8ff05
MistbornShadow <64308098+MistbornShadow@users.noreply.github.com>
2021-11-22 20:13:11 -0500

    Changes to Communications

--- app/src/main/java/com/example/yeehawholdem/LogicGoods/Communications.kt
+++ app/src/main/java/com/example/yeehawholdem/LogicGoods/Communications.kt
@@ -1,20 +1,32 @@
 package com.example.yeehawholdem.LogicGoods
 
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.room.Database
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.*
 import com.google.firebase.auth.ktx.auth
-import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.*
 
 class Communications {
-    lateinit var databaseReference : DatabaseReference
     lateinit var auth: FirebaseAuth
 
-    suspend fun getBet(){
-        val database = Firebase.database
-        auth = Firebase.auth
+    fun addEventListener(lobby: String) {
+        val dbReference = Firebase.database.getReference(lobby)
+        dbReference.child("Bet").addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val user = dataSnapshot.getValue(UserInfo::class.java)
 
+                // Check for null
+                if (user == null) {
+                    return
+                }
+            }
 
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+            }
+        })
     }
 }
